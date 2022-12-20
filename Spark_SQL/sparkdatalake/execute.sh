dde88cc2bfa6f8fb7e162721558c0f58e90fafef
Ramisous <90857017+Ramisous@users.noreply.github.com>
2022-12-20 19:19:53 +0100

    Update execute.sh

--- Spark_SQL/sparkdatalake/execute.sh
+++ Spark_SQL/sparkdatalake/execute.sh
@@ -1,4 +1,12 @@
-echo "Executing script"
-echo ""
+if [ -d "spark-warehouse" ]
+then 		
+	echo ""
+	echo "Removing previously existing spark-warehouse"
+	echo ""
+	rm -r "spark-warehouse"
+else
+	echo "Executing script"
+	echo ""
+fi
 
 spark-shell -i make_datalake.scala
